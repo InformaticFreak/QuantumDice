72d59c2d207d12fcf9998bfec5643420085ccb30
Fabian <fabianbartl.2003@outlook.de>
2021-05-27 12:25:40 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -28,35 +28,44 @@ class MyClient(discord.Client):
 	# recognize command
 	async def on_message(self, message):
 		if message.author != client.user and message.content.startswith(PREFIX):
+			print(message.content.strip())
 			
 			# roll command
-			if re.search(rf"{PREFIX}q?roll *[0-9]+ *d *[0-9]+ *(\* *[0-9]+)? *", message.content):
-				# extract mode
-				msg = message.content.replace(" ", "")
-				if message.content[len(PREFIX)] == "q":
-					quantum_mode = True
-					msg = msg.replace("q", "")
-				else:
-					quantum_mode = False
-				msg = f"{msg[len(PREFIX)+4:]}*1"
+			if re.search(rf"{PREFIX}q?roll [0-9]+d[0-9]+(\*[0-9]+)?( (\-min|\-max|-normal)?)? *", message.content):
+				msg = message.content.split()
+				# extract quantum mode
+				mode_quantum = False
+				if msg[0][len(PREFIX)] == "q":
+					mode_quantum = True
 				# extract dice data
-				count = int(msg.split("d")[0])
-				faces = int(msg.split("d")[1].split("*")[0])
-				factor = int(msg.split("*")[1])
+				msg[1] += "*1"
+				count = int(msg[1].split("d")[0])
+				faces = int(msg[1].split("d")[1].split("*")[0])
+				factor = int(msg[1].split("*")[1])
+				# extract min/max mode
+				msg.append("-normal")
+				mode_min_max = str(msg[2].strip("-"))
 				# generate dice roll
-				if quantum_mode:
+				if mode_quantum:
 					roll = [ round(qrng.get_random_double(1,faces)) for _ in range(count) ]
 				else:
 					roll = [ secrets.randbelow(faces)+1 for _ in range(count) ]
 				# get result
 				result = sum(roll) * factor
 				# create embed
-				if quantum_mode:
+				if mode_quantum:
 					embed = discord.Embed(title="Quantum Iacta Est", color=GOLD)
 				else:
 					embed = discord.Embed(title="Alea Iacta Est", color=BLUE)
 				embed.add_field(name="User", value=f"`{message.author.name}#{message.author.discriminator}`")
-				embed.add_field(name="Roll", value=f"`{roll}`")
+				if mode_min_max == "min":
+					embed.add_field(name="Min Roll", value=f"`{min(roll)}`")
+					result = min(roll) * factor
+				elif mode_min_max == "max":
+					embed.add_field(name="Max Roll", value=f"`{max(roll)}`")
+					result = max(roll) * factor
+				else:
+					embed.add_field(name="Roll", value=f"`{roll}`")
 				embed.add_field(name="Result", value=f"`{result}`")
 				await message.channel.send(embed=embed)
 			
