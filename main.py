9c952b96a4669eb5f49ec87c28303ac864b72180
Fabian <fabianbartl.2003@outlook.de>
2021-05-30 23:39:33 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class MyClient(discord.Client):
 	
 	# recognize command
 	async def on_message(self, message):
-		if message.author != client.user and message.content.startswith(PREFIX):
+		if not message.author is client.user and message.content.startswith(PREFIX):
 			
 			# roll command
 			if re.search(rf"{PREFIX}q?roll [0-9]+d[0-9]+(\*[0-9]+)?( (min|max|normal)?)? *", message.content):
